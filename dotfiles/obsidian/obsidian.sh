fd7842b46c95f4a81e8c9f1d90eaf9c87994509e
shubymao <shuby.mao@gmail.com>
2023-08-13 01:29:27 -0700

    initial changes

---
+++ dotfiles/obsidian/obsidian.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+# ensure ssh agent is running and run ssh add
+if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+	eval $(ssh-agent)
+	ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l >/dev/null || ssh-add
+
+obsidian.AppImage
