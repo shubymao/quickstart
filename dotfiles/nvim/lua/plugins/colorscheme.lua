4578bbbf3a991a0b47064997d3a95f6029e269c6
shubymao <shuby.mao@gmail.com>
2023-08-13 23:47:40 -0700

    initialized i3, polybar, rofi, alacritty

---
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent",
+      -- },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
