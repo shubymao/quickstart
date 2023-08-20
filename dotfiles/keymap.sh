d326b38084ce01606b777e0ca730c5bd8c8e8323
shubymao <shuby.mao@gmail.com>
2023-08-20 12:53:39 -0700

    added cursor and more config for i3

--- dotfiles/keymap.sh
+++ dotfiles/keymap.sh
@@ -1,10 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+sleep 5
+echo "pkill"
 pkill -9 xcape
+echo "layhout "
 setxkbmap -layout us
+echo "no cap"
 setxkbmap -option ctrl:nocaps
+echo "xcape 1"
 xcape -e 'Control_L=Escape'
+echo "remove mod4 hyper l"
 xmodmap -e "remove mod4 = Hyper_L"
+echo "mod 1 add hyper l"
 xmodmap -e "add mod1 = Hyper_L"
+echo "key 65 as hyper l"
 xmodmap -e "keycode 65 = Hyper_L"
+echo "any key as space"
 xmodmap -e "keycode any = space"
+echo "hyper l = space xcape"
 xcape -e "Hyper_L=space"
