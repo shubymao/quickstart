f462f13ea276b49450dcee25f81710866cb7cb92
Shuby Mao <shuby.mao@gmail.com>
2023-08-24 00:37:55 -0700

    updated the debian side of thing to be leaner and less likely to break

--- dotfiles/keymap.sh
+++ dotfiles/keymap.sh
@@ -8,14 +8,4 @@ setxkbmap -layout us
 echo "no cap"
 setxkbmap -option ctrl:nocaps
 echo "xcape 1"
-xcape -e 'Control_L=Escape'
-echo "remove mod4 hyper l"
-xmodmap -e "remove mod4 = Hyper_L"
-echo "mod 1 add hyper l"
-xmodmap -e "add mod1 = Hyper_L"
-echo "key 65 as hyper l"
-xmodmap -e "keycode 65 = Hyper_L"
-echo "any key as space"
-xmodmap -e "keycode any = space"
-echo "hyper l = space xcape"
-xcape -e "Hyper_L=space"
+xcape -e 'control_l=escape'
