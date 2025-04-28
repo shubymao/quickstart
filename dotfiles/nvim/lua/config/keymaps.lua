2b8bafe6ee2f0defb48cf0a7382aa362102d2463
shubymao <shuby.mao@gmail.com>
2025-04-27 21:40:24 -0700

    drafted a diff for blink with spell but didn't work

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -7,6 +7,7 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("t", "jk", "<C-\\><C-n>", { desc = "exit terminal mode with jk" }) -- decrement
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
